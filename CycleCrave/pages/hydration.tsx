2db6281dcf059d1ddf19f2c828e8f210828dbe72
Gabrielle Palar <gabrielle.a.palar@gmail.com>
2024-02-08 18:15:43 -0800

    cleaned up aligmnent, added use states for buttons to increment progress bar

--- CycleCrave/pages/hydration.tsx
+++ CycleCrave/pages/hydration.tsx
@@ -5,43 +5,42 @@ import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-nativ
 import CircularProgress from 'react-native-circular-progress-indicator';
 
 const hydration = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [progressValue, setProgressValue] = useState(0);
 
-    const handleSignUp = () => {
-        console.log('Email:', email, 'Password:', password);
+
+    const handleSignUp = (amount) => {
+        setProgressValue(progressValue + amount);
     };
 
     return (
         <View style={styles.container}>
             <View style={styles.upperContainer}>
                 <CircularProgress
-                    value={75}
+                    value={progressValue}
                     radius={90}
-                    duration={2000}
+                    duration={500}
                     progressValueColor={'black'}
                     maxValue={100}
                     title={'oz'}
-                    valueSuffix={'%'}
                     titleColor={'black'}
                     titleStyle={{ fontWeight: 'bold' }}
                 />
             </View>
             <View style={styles.lowerContainer}>
                 <View style={styles.buttonRow}>
-                    <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+                    <TouchableOpacity style={[styles.button, styles.lightBlue]} onPress={() => handleSignUp(8)}>
                         <Text>8 oz</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+                    <TouchableOpacity style={[styles.button, styles.mediumBlue]} onPress={() => handleSignUp(16)}>
                         <Text>16 oz</Text>
                     </TouchableOpacity>
                 </View>
 
                 <View style={styles.buttonRow}>
-                    <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+                    <TouchableOpacity style={[styles.button, styles.darkBlue]} onPress={() => handleSignUp(32)}>
                         <Text>32 oz</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+                    <TouchableOpacity style={[styles.button, styles.darkestBlue]} onPress={() => handleSignUp(64)}>
                         <Text>64 oz</Text>
                     </TouchableOpacity>
                 </View>
@@ -66,6 +65,8 @@ const styles = StyleSheet.create({
         margin: 10,
     },
     button: {
+        paddingVertical: 15, // Increase vertical padding to make the button taller
+        paddingHorizontal: 30, // Increase horizontal padding to make the button wider
         width: '45%',
         padding: 8,
         margin: 8,
@@ -73,22 +74,39 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         borderWidth: 1,
         backgroundColor: "#FEDBD5",
-        borderColor: "#FF898D",
         flex: 1,
         marginHorizontal: 5,
         minWidth: 100, // Set a minimum width for the button
+
     },
 
     upperContainer: {
         flex: 6, // takes 2/3 of the screen
-        // width: 50
+        paddingTop: 100,
+        justifyContent: 'center'
     },
 
     lowerContainer: {
         flex: 4, // takes 1/3 of the screen
         justifyContent: 'flex-end', // align content to the bottom
-        paddingBottom: 300, // add some padding to create space between the buttons and the bottom
-    }
+        paddingBottom: 200, // add some padding to create space between the buttons and the bottom
+    },
+    lightBlue: {
+        backgroundColor: "#EEF7FF", // Light blue
+        borderColor: "#EEF7FF", // Light blue
+    },
+    mediumBlue: {
+        backgroundColor: "#D2EBFF", // Medium blue
+        borderColor: "#D2EBFF", // Light blue
+    },
+    darkBlue: {
+        backgroundColor: "#A5D7FF", // Dark blue
+        borderColor: "#A5D7FF", // Light blue
+    },
+    darkestBlue: {
+        backgroundColor: "#75C1FF", // Darkest blue
+        borderColor: "#75C1FF", // Light blue
+    },
 
 });
 
