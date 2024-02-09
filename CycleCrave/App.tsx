7d3851c2555d87e32f1fa11fa05f7060261fe3e1
Gabrielle Palar <gabrielle.a.palar@gmail.com>
2024-02-08 17:46:48 -0800

    additional changed pages

--- CycleCrave/App.tsx
+++ CycleCrave/App.tsx
@@ -1,16 +1,21 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import SignUp from './pages/signup'
+import SignUp from './pages/SignUp'
+import hydration from './pages/hydration'
+
+import { PaperProvider } from 'react-native-paper';
 
 const Stack = createNativeStackNavigator()
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="SignUp" component={SignUp} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="hydration" component={hydration} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   )
 }
 
